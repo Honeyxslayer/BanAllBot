6f59e75af42059949aa94c2d4385b4c5ee569e1f
Honeyxslayer <163923981+Honeyxslayer@users.noreply.github.com>
2024-05-21 23:31:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 class Config:
-    API_ID=24509589
-    API_HASH="717cf21d94c4934bcbe1eaa1ad86ae75"
-    TOKEN="6572403691:AAFV-3PpCb7N06uqJC2mCdK4QzW6VtV5CeE"
-    SUDO = list(int(i) for i in os.environ.get("SUDO", "6664582540").split(" "))
-    START_IMG="https://telegra.ph/file/52fefb8bd51289a83a49b.jpg"
-    BOT_ID=6796545941
-    BOT_USERNAME="ZuliaiBot"
-    BOT_NAME="ZuliaiBot"
+    API_ID=29044160
+    API_HASH="b93797389eab3cec8c697ae4f2418466"
+    TOKEN="6356760944:AAG3iXHNJdQ1pL1yWZRrwf_gDXdcFhs9f20"
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "5934011554").split(" "))
+    START_IMG="https://telegra.ph/file/dd95304112d1a99b35891.jpg"
+    BOT_ID=6356760944
+    BOT_USERNAME="Insanexbanallbot"
+    BOT_NAME="insanexbanallllll"
